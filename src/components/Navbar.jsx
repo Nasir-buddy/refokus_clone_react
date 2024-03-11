0370fa2a5f634d81417608771a8d22d0deca45e2
nasir-buddy <syedali172000@gmail.com>
2024-03-12 03:14:49 +0530

    Refokus: 'Implementation of Navbar'

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='max-w-screen-xl m-auto bg-zinc-800 py-6 text-white flex items-center'>
+            <img src="https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63349803431f1562dccf1802_refokus%20logo.svg" alt="" />
+            <div className='flex gap-10 ml-20'>
+                {["Home", "Work", "Culture"].map((item, index) => {
+                    return <a href='#' className='text-sm flex items-center gap-1'>
+                        {(index === 1) && 
+                            <span style={{boxShadow: "0 0 0.25em #00FF19"}} 
+                            className='inline-block h-1 w-1 bg-green-500 rounded-full'>
+                        </span>}
+                        {item}
+                    </a>
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
