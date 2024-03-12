c2aab16ab086bcdd250df39008f82ce99c0fa3d2
nasir-buddy <syedali172000@gmail.com>
2024-03-12 16:10:39 +0530

    Refokus: 'Implemented navbar section'

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,9 @@ const Navbar = () => {
         <div className='max-w-screen-xl m-auto bg-zinc-800 py-6 text-white flex items-center'>
             <img src="https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63349803431f1562dccf1802_refokus%20logo.svg" alt="" />
             <div className='flex gap-10 ml-20'>
-                {["Home", "Work", "Culture"].map((item, index) => {
-                    return <a href='#' className='text-sm flex items-center gap-1'>
+                {["Home", "Work", "Culture", "", "News"].map((item, index) => {
+                    return item.length === 0 ? <span className='h-7 w-[2px] bg-zinc-700'></span> : 
+                    <a href='#' className='text-sm flex items-center gap-1'>
                         {(index === 1) && 
                             <span style={{boxShadow: "0 0 0.25em #00FF19"}} 
                             className='inline-block h-1 w-1 bg-green-500 rounded-full'>
