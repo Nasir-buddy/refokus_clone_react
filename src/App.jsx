65f0f4583b24c78b96e90c18659c43d7c40880f0
nasir-buddy <syedali172000@gmail.com>
2024-03-13 01:37:32 +0530

    Refokus: 'Fix height issue from app.jsx'

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Work from './components/Work'
 import Strips from './components/Strips'
+import Products from './components/Products'
 
 function App() {
   return (
-    <div className='bg-zinc-900 h-screen w-screen text-white font-["satoshi"]'>
+    <div className='bg-zinc-900 w-screen text-white font-["satoshi"]'>
         <Navbar />
         <Work />
         <Strips />
+        <Products />
     </div>
   )
 }
