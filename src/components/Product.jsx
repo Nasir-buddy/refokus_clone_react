7dc786494dbec1097277f09f300a202ccc2ef69c
nasir-buddy <syedali172000@gmail.com>
2024-03-13 03:21:03 +0530

    Refokus: 'fix some sizing of text in product component'

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,10 +5,13 @@ function Product({val}) {
   return (
     <div className='w-full py-20 text-white '>
         <div className='max-w-screen-xl mx-auto flex items-center justify-between'>
-            <h1 className='text-6xl capitalize font-semibold'>{val.name}</h1>
+            <h1 className='text-6xl capitalize font-regular'>{val.name}</h1>
             <div className='w-1/3'>
-                <p className='mb-10'>{val.description}</p>
-                <Button />
+                <p className='mb-10 text-[1.1rem]'>{val.description}</p>
+                <div className='flex items-center gap-10'>
+                {val.live && <Button />}
+                {val.case && <Button title={'case study'}/>}
+                </div>
             </div>
         </div>
     </div>
