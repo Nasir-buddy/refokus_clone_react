5edf9c69d75f3230c1f01c1e1eda53ddd6694108
nasir-buddy <syedali172000@gmail.com>
2024-03-13 02:14:07 +0530

    Refokus: 'fix button and get data from another component'

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { IoIosReturnRight } from "react-icons/io";
-function Button() {
+function Button({title = "Get Started"}) {
   return (
-    <div className='min-w-40 bg-zinc-100 text-black rounded-full px-4 py-2 flex items-center justify-between '>
-    <span className='text-sm font-medium'>Get Started</span>
+    <div className='min-w-[7.5vw] bg-zinc-100 text-black rounded-full px-4 py-2 flex items-center justify-between '>
+    <span className='text-sm font-medium'>{title}</span>
     <IoIosReturnRight />
     </div>
   )
