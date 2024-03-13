bb0c51ef8310b41b04f370c3fd843b9c0123bf6c
nasir-buddy <syedali172000@gmail.com>
2024-03-14 03:23:24 +0530

    Refokus: implement product on hover effect

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Button from './Button'
 
-function Product({val}) {
+function Product({val, mover, index}) {
   return (
-    <div className='w-full py-20 text-white '>
-        <div className='max-w-screen-xl mx-auto flex items-center justify-between'>
+    <div className='w-full py-20 text-white h-[23rem]'>
+        <div onMouseEnter={()=>{mover(index)}} className='max-w-screen-xl mx-auto flex items-center justify-between'>
             <h1 className='text-6xl capitalize font-regular'>{val.name}</h1>
             <div className='w-1/3'>
                 <p className='mb-10 text-[1.1rem]'>{val.description}</p>
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Product from './Product'
+import { motion } from 'framer-motion';
 
 function Products() {
     const products = [
@@ -13,13 +14,13 @@ function Products() {
             name: "Cula",
             description: "We immersed ourselves in a 3D world we created to explain how Cula's platform collects data from carbon removal processes and converts them into carbon credit certificates.",
             live: true,
-            case: false
+            case: true
         },
         {
             name: "TTR",
             description: "We've created an interactive site using generative AI to allow users to engage with our thinking about Ai, industry trends and design.",
             live: true,
-            case: false
+            case: true
         },
         {
             name: "Maniv",
@@ -31,14 +32,31 @@ function Products() {
             name: "YIR 2022",
             description: "Our second year was filled with great events, exciting projects, awards and amazing people - so we made another showcase to celebrate.",
             live: true,
-            case: false
+            case: true
         },
-    ]
+    ];
+    
+    const [position , setPosition] = useState();
+    const mover = (value) => {
+        setPosition(value * 23);
+    }
     return (
-        <div>
+        <div className='relative'>
             {products.map((item, index)=>(
-                <Product  val={item}/>
+                <Product val={item} index={index} mover={mover}/>
             ))}
+            <div className='absolute w-full h-full top-0 pointer-events-none '>
+                <motion.div
+                initial={{y: position, x: "-50%"}}
+                animate={{y: position + `rem`}}
+                className='window absolute w-[32rem] h-[23rem] bg-white left-[42.5%] overflow-hidden'>
+                    <div className='w-full h-full bg-red-900 '></div>
+                    <div className='w-full h-full bg-pink-200 '></div>
+                    <div className='w-full h-full bg-sky-300 '></div>
+                    <div className='w-full h-full bg-sky-400 '></div>
+                    <div className='w-full h-full bg-sky-500 '></div>
+                </motion.div>
+            </div>
         </div>
     )
 }
