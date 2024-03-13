deaa6042ffe859062e9fa6f7ad550f2d76e5149c
nasir-buddy <syedali172000@gmail.com>
2024-03-13 17:16:02 +0530

    Refokus: 'implemented the footer component'

---
+++ src/components/Footer.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className='max-w-screen-xl mx-auto flex items-center gap-20'>
+            <div className='w-[50%]'>
+                <h1 className='text-[9rem] py-24 font-semibold'>Refokus.</h1>
+            </div>
+            <div className='flex flex-col '>
+                <h1 className='text-xl mb-9'>Social</h1>
+                {["social", "instagram", "twitter (x?)", "linkedin",].map((item, index) => (
+                    <a className='text-zinc-500 mt-3' href="">{item}</a>
+                ))}
+            </div>
+            <div className='flex flex-col '>
+                <h1 className='text-xl mb-9'>Social</h1>
+                {["social", "instagram", "twitter (x?)", "linkedin",].map((item, index) => (
+                    <a className='text-zinc-500 mt-3' href="">{item}</a>
+                ))}
+            </div>
+            <div className='max-w-[20%] mt-0 text-right '><p className='mr-16'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste accusantium nemo, ab nesciunt dolor aut.
+            </p>
+            
+            </div>
+        </div>
+    )
+}
+
+export default Footer
